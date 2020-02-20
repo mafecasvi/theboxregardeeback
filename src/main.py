8a4f4339e374718c4981adf96d5e2b45a18aaaa7
mafecasvi <mafecasvi@gmail.com>
2020-02-20 00:06:50 +0000

    commit del backend para crear usuarios, hijos y ppto

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+import json
+from flask import Flask, request, jsonify, url_for, make_response
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, User, Budget, Children
 #from models import Person
 
 app = Flask(__name__)
@@ -28,14 +29,91 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/profilecreation', methods=['POST'])
+@app.route('/profilecreation/<user_email>', methods=["GET"])
+def handle_profilecreation(user_email=""):
 
     response_body = {
         "hello": "world"
     }
+    # requesting_user = User.query.filter_by(id=username).all()
+    posting_user_email = request.json["email"]
+    if request.method == "GET":
+        requesting_user = User.query.filter_by(email=user_email).one_or_none()
+    else:
+        requesting_user = User.query.filter_by(email=posting_user_email).one_or_none()
+    
+    print(requesting_user)
+    if request.method == "GET":
+        
+        if user_email:
+            
+            if requesting_user:
+                # user exists
+                status_code = 200
+                response_body = {
+                    "result": "El usuario esta registrado"
+                }
+            else:
+                # user does not exist
+                status_code = 404
+                response_body = {
+                    "result": "El usuario no existe"
+                }
+        else:
+            # bad request no user email on url
+            status_code = 400
+            response_body = {
+                "result": "No hay user email en la url"
+            }
 
-    return jsonify(response_body), 200
+    elif request.method == "POST":
+        new_user_data = request.json
+        if requesting_user:
+            # actualizo los datos porque existe
+            requesting_user.update(new_user_data)
+            owner_user_id = requesting_user.id
+            Budget.query.filter_by(user_userid=requesting_user.id).delete()
+            Children.query.filter_by(user_userid=requesting_user.id).delete()
+
+        else:
+            # creo al nuevo usuario
+            print("trying to create")
+            new_user = User(new_user_data)
+            db.session.add(new_user)
+            
+            db.session.commit()
+            owner_user_id = new_user.id
+        
+        # try:
+            
+        status_code = 201
+        print("first commit success")
+        
+        # create budgets
+        budget = Budget(new_user_data["thebudget"], owner_user_id)
+        db.session.add(budget)
+        children_data = new_user_data["thechildren"]
+        for child in new_user_data["thechildren"]:
+            new_child = Children(child, owner_user_id)
+            db.session.add(new_child)
+        
+
+        result = "Ya estás anotado para la próxima entrega de The Box Regardée" 
+        response_body = {
+            "result": result
+        }
+        # except:
+        #    print('explote')
+        #    status_code = 400
+        #    response_body = {
+        #        "result": "La info no fue almacenada"
+        #    }
+        db.session.commit()
+    return make_response(
+    jsonify(response_body),
+    status_code)
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,17 +1,197 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
+import json
+from datetime import datetime
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Gender(Enum):
+    WOMAN = "woman"
+    MAN = "man"
+    OTHER = "other"
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+class Arms(Enum):
+    SHORT = "short"
+    AVERAGE = "average"
+    LONGY = "long"
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+class Shoulders(Enum):
+    NARROW = 'narrow'
+    AVERAGE = 'average'
+    WIDE = 'wide'
+
+class Torso(Enum):
+    SHORT = 'short'
+    AVERAGE = 'average'
+    LONGY = 'long'
+
+class Hips(Enum):
+    NARROW = 'narrow'
+    AVERAGE = 'average'
+    WIDE = 'wide'
+
+class Legs(Enum):
+    SHORT = 'short'
+    AVERAGE = 'average'
+    LONGY = 'long'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    genderuser = db.Column(db.Enum(Gender), nullable=False)
+    # look = db.Column(db.Enum(Lookchoice), nullable=False)
+    datecreated = db.Column(db.DateTime, nullable=False)
+    username = db.Column(db.String(12), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    birthdate = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    lastname = db.Column(db.String(120), nullable=False)
+    mobilenumber = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(280), nullable=False)
+    socialmedia = db.Column(db.String(280))
+    sizetop = db.Column(db.String(280), nullable=False)
+    sizebottom = db.Column(db.String(280), nullable=False)
+    sizeshoes = db.Column(db.String(280), nullable=False)
+    igaccount = db.Column(db.String(140), nullable=True)
+    height = db.Column(db.Integer, nullable=False)
+    waist = db.Column(db.Integer, nullable=False)
+    armsuser = db.Column(db.Enum(Arms), nullable=False)
+    shouldersuser = db.Column(db.Enum(Shoulders), nullable=False)
+    torsouser = db.Column(db.Enum(Torso), nullable=False)
+    hipsuser = db.Column(db.Enum(Hips), nullable=False)
+    legsuser = db.Column(db.Enum(Legs), nullable=False)
+    thebudget = db.relationship("Budget", back_populates="user", uselist=False)
+    thechildren = db.relationship("Children", back_populates="user")
+
+    def __init__(self, new_user_data):
+        self.genderuser = new_user_data["genderuser"]
+        self.datecreated = datetime.strptime(new_user_data["datecreated"], "%Y/%m/%d")
+        self.username = new_user_data["username"]
+        self.email = new_user_data["email"]
+        self.birthdate = datetime.strptime(new_user_data["birthdate"], "%Y/%m/%d")
+        self.name = new_user_data["name"]
+        self.lastname = new_user_data["lastname"]
+        self.mobilenumber = new_user_data["mobilenumber"]
+        self.address = new_user_data["address"]
+        self.socialmedia = new_user_data["socialmedia"]
+        self.sizetop = new_user_data["sizetop"]
+        self.sizebottom = new_user_data["sizebottom"]
+        self.sizeshoes = new_user_data["sizeshoes"]
+        self.igaccount = new_user_data["igaccount"]
+        self.height = int(new_user_data["height"])
+        self.waist = int(new_user_data["waist"])
+        self.armsuser = new_user_data["armsuser"]
+        self.shouldersuser = new_user_data["shouldersuser"]
+        self.torsouser = new_user_data["torsouser"]
+        self.hipsuser = new_user_data["hipsuser"]
+        self.legsuser = new_user_data["legsuser"]
+        # self.thebudget = new_user_data["thebudget"]
+        # self.thechildren = new_user_data["thechildren"]
+        
+    def serialize(self):
+
+        return {
+            "id": self.id,
+            "genderuser": self.genderuser,
+            "datecreated": self.datecreated,
+            "username": self.username,
+            "email": self.email,
+            "age": datetime.today() - self.birthdate,
+            "name": self.name,
+            "lastname": self.lastname,
+            "mobilenumber": self.mobilenumber,
+            "address": self.address,
+            "socialmedia": self.socialmedia,
+            "sizetop": self.sizetop,
+            "sizebottom": self.sizebottom,
+            "sizeshoes": self.sizeshoes,
+            "igaccount": self.igaccount,
+            "height": self.height,
+            "waist": self.waist,
+            "armsuser": self.armsuser,
+            "shouldersuser": self.shouldersuser,
+            "torsouser": self.torsouser,
+            "hipsuser": self.hipsuser,
+            "legsuser": self.legsuser,
+            "budget": [budget.serialize() for budget in self.thebudget],
+            "children": [children.serialize() for children in self.thechildren]
+            }
+
+    def update(self, new_user_data):
+        """ actualiza la informacion del usuario """
+        self.email = new_user_data["email"]
+        self.mobilenumber = new_user_data["mobilenumber"]
+        self.address = new_user_data["address"]
+        self.socialmedia = new_user_data["socialmedia"]
+        self.sizetop = new_user_data["sizetop"]
+        self.sizebottom = new_user_data["sizebottom"]
+        self.sizeshoes = new_user_data["sizeshoes"]
+        self.igaccount = new_user_data["igaccount"]
+        self.waist = new_user_data["waist"]
+        self.hipsuser = new_user_data["hipsuser"]
+        self.budgettop = new_user_data["budgettop"]
+        self.budgetbottom = new_user_data["budgetbottom"]
+        self.budgetshoes = new_user_data["budgetshoes"]
+        self.budgetbag = new_user_data["budgetbag"]
+        self.budgetaccesories = new_user_data["budgetaccesories"]
+
+
+class Lookchoice(db.Model):
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    look_url = db.Column(db.String(400), nullable=False)
+    lookorder = db.Column(db.Integer, nullable=False)
+
+class Dislikes(db.Model):
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+
+class Occasions(db.Model):
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+
+class Jeans(db.Model):
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    jeanschoiceurl = db.Column(db.String(400), nullable=False)
+
+class Fittop(db.Model):
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    fittoporder = db.Column(db.Integer, nullable=False)
+
+class Fitbottom(db.Model):
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    fitbottomorder = db.Column(db.Integer, nullable=False)
+
+class Budget(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    budgettop = db.Column(db.Integer, nullable=True)
+    budgetbottom = db.Column(db.Integer, nullable=True)
+    budgetshoes = db.Column(db.Integer, nullable=True)
+    budgetsportshoes = db.Column(db.Integer, nullable=True)
+    budgetbag = db.Column(db.Integer, nullable=True)
+    budgetaccesories = db.Column(db.Integer, nullable=True)
+    user = db.relationship("User", back_populates="thebudget")
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, new_user_data, user_id):
+        self.budgettop = new_user_data["budgettop"]
+        self.budgetbottom = new_user_data["budgetbottom"]
+        self.budgetshoes = new_user_data["budgetshoes"]
+        self.budgetbag = new_user_data["budgetbag"]
+        self.budgetaccesories = new_user_data["budgetaccesories"]
+        self.user_userid = user_id
+
+class Children(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    birthdate = db.Column(db.DateTime, nullable=False)
+    genderuser = db.Column(db.Enum(Gender), nullable=False)
+    user = db.relationship("User", back_populates="thechildren")
+    user_userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, children_data, user_id):
+        self.birthdate = datetime.strptime(children_data["birthdate"], "%Y/%m/%d")
+        self.genderuser = children_data["genderuser"]
+        self.user_userid = user_id
+
